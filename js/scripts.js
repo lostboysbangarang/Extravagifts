31924065369a69cb502a4b61d77d07a1564acbad
Hayden Lugo <haydenlugo96@gmail.com>
2021-04-23 23:29:40 -0400

    figured out a easy way to add collections

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,193 @@
-var i=0;
+var slip = [];
+// var i=0;
+var index=0;
 var images=[];
-var time=10000;
+var urls=[];
+var critters=[];
+var cages=[];
+var sent=[];
+var idk=[];
+var timer;
+var time=3000;
+var timeOut=0;
 var base=window.location.pathname;
+var xhr=new XMLHttpRequest();
 console.log(base);
 console.log(window.location.pathname);
-for (j=0; j < 15; j++) {
-    uj=j+1;
-    url="resources/cats/bear/dogs/bark/"+uj+".png";
-    images[j]=url;
-    console.log (images[j]);
+
+
+
+
+
+urls[0]="bear";
+urls[1]="dub"
+function hug(you, me){
+    for(let y=0, len=you.length; y< len; y++){
+        me.push(you[y]);
+    }
+    console.log(me);
+    return me;
+    
 }
-function slideToTheLeft(){
-    I=i
-    II=i+1
-    III=i+2
-    if (typeof images[III] != "undefined") {
-        document.getElementsByClassName("photo")[2].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+images[III]+")";
-        // document.getElementsByClassName("photo")[2].style.boxShadow="2.5px 2.5px 2.5px black inset, -2.5px -2.5px 2.5px black inset, -2.5px 2.5px 2.5px black inset, 2.5px -2.5px 2.5px black inset";
-    } else {
-        document.getElementsByClassName("photo")[2].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+images[1]+")";
-    }
-    if (typeof images[II] != "undefined") {
-        document.getElementsByClassName("photo")[1].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+images[II]+")";
-    } else {
-        document.getElementsByClassName("photo")[1].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+images[0]+")";
-    }
-    document.getElementsByClassName("photo")[0].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+images[I]+")";
-    if (i <images.length-1) {
-        i++;
-    } else {
-        i=0;
-    }
-    setTimeout("slideToTheLeft()", time);
+function sigh() {
+    console.log("K:  "+k);
+    // slip=cages[k];
+    // console.log(slip);
+    timer=setInterval(slideToTheLeft(slip, 0),time);
+    // slideToTheLeft(slip);
 }
-window.onload = slideToTheLeft;
+function sweet(){
+    console.log("k:  "+k);
+    if (timeOut<cages[k].length) {
+        slideToTheLeft(slip);
+    }
+}
+function slideImproved() {
+    for (j=0; j<urls.length; j++) {
+        urls[j]="resources/cats/"+urls[j]+"/dogs/bark/";
+        for (k=0; k<100; k++) {
+            kj=k+1;
+            url=""+urls[j]+""+kj+".png";
+            console.log(url);
+            xhr.open("HEAD",url,false);
+            xhr.send();
+            if (xhr.status !== 404){
+
+
+                critters[k]=url;
+                console.log(critters[k]);
+            } else {
+                k=101;
+                console.log("Nice try kid");
+                cages[j]=critters;
+                critters=[];
+            }
+        }
+
+    }
+    // slideToTheLeft(cages);
+    
+    for (k=0; k<urls.length; k++) {
+        idk=cages[k];
+        hug(idk,slip);
+        // console.log(cages);
+        console.log(slip[0]);
+        console.log("K:   "+k);
+        // sweet();
+        // setTimeout("sigh()", time*cages[k].length);
+        // setInterval(sweet, time);
+        // slip=cages[k];
+        // console.log(slip);
+        // slideToTheLeft(slip);
+    }
+    // sweet();
+    sigh();
+}
+
+
+
+
+// function slideToTheLeft(index){
+//     console.log("index:   "+index)
+//     if (l<kennel.length) {
+//         l=0;
+//     }
+//     console.log(kennel.length);
+//     console.log("l:   "+l)
+//     // for (l=0; l<kennel.length; l++) {
+
+//         I=i;
+//         II=i+1;
+//         III=i+2;
+//         if (typeof kennel[I] != "undefined") {
+//             console.log(kennel.length);
+//             setTimeout (function() {
+//                 console.log(kennel.length);
+//                 if (typeof kennel[III] != "undefined") {
+//                     document.getElementsByClassName("photo")[2].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[III]+")";
+//                     // document.getElementsByClassName("photo")[2].style.boxShadow="2.5px 2.5px 2.5px black inset, -2.5px -2.5px 2.5px black inset, -2.5px 2.5px 2.5px black inset, 2.5px -2.5px 2.5px black inset";
+//                 } else {
+//                     document.getElementsByClassName("photo")[2].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[1]+")";
+//                 }
+//                 if (typeof kennel[II] != "undefined") {
+//                     document.getElementsByClassName("photo")[1].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[II]+")";
+//                 } else {
+//                     document.getElementsByClassName("photo")[1].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[0]+")";
+//                 }
+//                 document.getElementsByClassName("photo")[0].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[I]+")";
+//                 if (i<kennel.length) {
+//                     i++;
+//                     // l++;
+//                     // setTimeout("slideToTheLeft()", time);
+//                 } else {
+//                     i=0;
+//                 }
+//             },time);
+
+//         }
+//     // }
+
+
+
+
+// }
+function slideToTheLeft(kennel, i){
+    
+        console.log("index:   "+index)
+        // if (l<kennel.length) {
+        //     l=0;
+        // }
+        console.log(kennel.length);
+        if (i==kennel.length){
+            i=0;
+        }
+        console.log("I:   "+i);
+        // console.log("l:   "+l)
+        // for (l=0; l<kennel.length; l++) {
+
+        I=i;
+        II=i+1;
+        III=i+2;
+        if (typeof kennel[I] != "undefined") {
+            console.log(kennel.length);
+
+            console.log(kennel.length);
+            if (typeof kennel[III] != "undefined") {
+                document.getElementsByClassName("photo")[2].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[III]+")";
+                console.log(window.location.origin+""+window.location.pathname+""+kennel[III]);
+                // document.getElementsByClassName("photo")[2].style.boxShadow="2.5px 2.5px 2.5px black inset, -2.5px -2.5px 2.5px black inset, -2.5px 2.5px 2.5px black inset, 2.5px -2.5px 2.5px black inset";
+            } else {
+                document.getElementsByClassName("photo")[2].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[1]+")";
+            }
+            if (typeof kennel[II] != "undefined") {
+                document.getElementsByClassName("photo")[1].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[II]+")";
+                console.log(window.location.origin+""+window.location.pathname+""+kennel[II]);
+            } else {
+                document.getElementsByClassName("photo")[1].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[0]+")";
+            }
+            document.getElementsByClassName("photo")[0].style.backgroundImage="url("+window.location.origin+""+window.location.pathname+""+kennel[I]+")";
+            console.log(window.location.origin+""+window.location.pathname+""+kennel[I]);
+            if (i<kennel.length) {
+                i=II;
+                // l++;
+                // timer=setInterval(slideToTheLeft(kennel),time);
+                setTimeout(slideToTheLeft, time, kennel, i);
+                // setTimeout(function() {
+                //     slideToTheLeft(kennel, i);
+                // }, time);
+            } else {
+                i=0;
+                slideToTheLeft(kennel, i);
+            }
+
+
+        }
+    
+    
+    // }
+
+
+
+
+}
+window.onload = slideImproved();
